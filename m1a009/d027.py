3fe50db8735be293f7747e6c8360e36e181aec5f
Walace Paz <walacepaz11@gmail.com>
2020-11-04 17:58:04 -0300

    correção de código

--- m1a009/d027.py
+++ m1a009/d027.py
@@ -1,4 +1,4 @@
 n = str(input('Digite seu nome completo: ')).strip().split()
 print('Muito prazer em te conhecer!')
 print('Seu primeiro nome é {}'.format(n[0]))
-print('Seu último nome é {}'.format(n[len(n) - 1]))
+print('Seu último nome é {}'.format(n[-1]))
